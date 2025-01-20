7fd31e45c851f6b3a3c17a0ebd9744dff8034701
Jon Alling <jon@humancrafted.co>
2025-01-20 15:31:21 -0600

    Jons-MacBook-M2-Pro.local 2025-1-20:15:31:20

--- content/about.md
+++ content/about.md
@@ -2,7 +2,6 @@
 title: About Us
 description: Learn more about Human Crafted, our mission, and our team.
 ---
-
 # About Human Crafted
 
 Human Crafted is a product design studio dedicated to bringing ideas to life through innovative design and rapid prototyping...
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"
+import { ThemeProvider } from "@/components/theme-provider"
+import { NavBar } from "@/components/nav-bar"
+import { Footer } from "@/components/footer"
+import "@/styles/globals.css"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
+
